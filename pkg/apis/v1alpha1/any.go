4f7743fa4fe822a0bfb2fe8a7adb1716ad86e6a1
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-04 00:10:31 +0200

    fix: custom marshalling for Any (#11)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/apis/v1alpha1/any.go
+++ pkg/apis/v1alpha1/any.go
@@ -1,22 +1,34 @@
 package v1alpha1
 
 import (
+	"encoding/json"
+
 	"github.com/jinzhu/copier"
 )
 
-type Value = interface{}
-
 // +k8s:deepcopy-gen=false
 type Any struct {
-	// TODO: this is needed to workaround a bug in api machinery code
-	// https://kubernetes.slack.com/archives/C0EG7JC6T/p1696331287543159
 	// +kubebuilder:pruning:PreserveUnknownFields
 	// +kubebuilder:validation:Schemaless
-	Value `json:",inline"`
+	Value interface{} `json:",inline"`
 }
 
 func (in *Any) DeepCopyInto(out *Any) {
 	if err := copier.Copy(out, in); err != nil {
 		panic("deep copy failed")
 	}
+}
+
+func (a *Any) MarshalJSON() ([]byte, error) {
+	return json.Marshal(a.Value)
+}
+
+func (a *Any) UnmarshalJSON(data []byte) error {
+	var v interface{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return err
+	}
+	a.Value = v
+	return nil
 }
