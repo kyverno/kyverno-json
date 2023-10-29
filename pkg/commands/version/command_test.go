e4b0f2e0b7fb8c7781a4a8c9c23bc552f52593e7
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-29 23:56:03 +0100

    refactor: move test files
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/version/command_test.go
+++ pkg/commands/version/command_test.go
@@ -3,67 +3,58 @@ package version
 import (
 	"bytes"
 	"io"
-	"strings"
+	"os"
 	"testing"
 
-	"github.com/kyverno/kyverno-json/pkg/version"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCommand(t *testing.T) {
-	version.BuildVersion = "test"
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetOut(b)
-	err := cmd.Execute()
-	assert.NoError(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	expected := `
-Version: test
-Time: ---
-Git commit ID: ---`
-	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(string(out)))
-}
-
-func TestCommandWithInvalidArg(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetErr(b)
-	cmd.SetArgs([]string{"foo"})
-	err := cmd.Execute()
-	assert.Error(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	expected := `Error: unknown command "foo" for "version"`
-	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(string(out)))
-}
-
-func TestCommandWithInvalidFlag(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetErr(b)
-	cmd.SetArgs([]string{"--xxx"})
-	err := cmd.Execute()
-	assert.Error(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	expected := `Error: unknown flag: --xxx`
-	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(string(out)))
-}
-
-func TestCommandHelp(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetOut(b)
-	cmd.SetArgs([]string{"--help"})
-	err := cmd.Execute()
-	assert.NoError(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	assert.True(t, strings.HasPrefix(string(out), cmd.Long))
+func Test_Execute(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		wantErr bool
+		out     string
+	}{{
+		name: "help",
+		args: []string{
+			"--help",
+		},
+		out:     "../../../test/commands/version/help.txt",
+		wantErr: false,
+	}, {
+		name:    "version",
+		out:     "../../../test/commands/version/out.txt",
+		wantErr: false,
+	}, {
+		name:    "unknow flag",
+		args:    []string{"--foo"},
+		wantErr: true,
+	}, {
+		name:    "unknow arg",
+		args:    []string{"foo"},
+		wantErr: true,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := Command()
+			assert.NotNil(t, cmd)
+			cmd.SetArgs(tt.args)
+			out := bytes.NewBufferString("")
+			cmd.SetOut(out)
+			err := cmd.Execute()
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			actual, err := io.ReadAll(out)
+			assert.NoError(t, err)
+			if tt.out != "" {
+				expected, err := os.ReadFile(tt.out)
+				assert.NoError(t, err)
+				assert.Equal(t, string(expected), string(actual))
+			}
+		})
+	}
 }
