9c25481b7ff097742a4fbc1bfb41c8a6bdc3fdc3
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-30 00:03:24 +0100

    feat: improve api (#157)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/apis/v1alpha1/assertion.go
+++ pkg/apis/v1alpha1/assertion.go
@@ -1,7 +1,9 @@
 package v1alpha1
 
+// Assertion contains an assertion tree associated with a message.
 type Assertion struct {
-	// Message is the variable associated message.
+	// Message is the message associated message.
+	// +optional
 	Message string `json:"message,omitempty"`
 
 	// Check is the assertion check definition.
