dd3b385e570cc0cd8a9e07c3108699eef00a402e
Vishal Choudhary <vishal.choudhary@nirmata.com>
2023-11-01 13:22:06 +0530

    feat: add tests for AWS ECS (#169)
    * feat: add tests for AWS ECS
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * fix: update message in validating policy
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * feat: linting
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    ---------
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    Co-authored-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ test/commands/scan/tf-ecs-task-definition/main.tf
@@ -0,0 +1,43 @@
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      readonlyRootFilesystem = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      readonlyRootFilesystem = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
