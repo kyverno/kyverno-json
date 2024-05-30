eabf0a07f674d1c2a68e57dc3f703e4cbd3b9eae
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-05-31 01:13:03 +0200

    fix tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/data/data_test.go
+++ pkg/data/data_test.go
@@ -11,15 +11,9 @@ func TestCrds(t *testing.T) {
 	data, err := Crds()
 	assert.NoError(t, err)
 	{
-		file, err := fs.Stat(data, "crds/json.kyverno.io_validatingpolicies.yaml")
+		file, err := fs.Stat(data, "json.kyverno.io_validatingpolicies.yaml")
 		assert.NoError(t, err)
 		assert.NotNil(t, file)
 		assert.False(t, file.IsDir())
-	}
-	{
-		file, err := fs.Stat(data, "crds")
-		assert.NoError(t, err)
-		assert.NotNil(t, file)
-		assert.True(t, file.IsDir())
 	}
 }
