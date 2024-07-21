0a1e5d80fd0ccdbbf0b3c58bd3831a8d53389b13
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-07-21 20:28:26 +0200

    fix: projecting over inexistent fields
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/assert/project_test.go
+++ pkg/engine/assert/project_test.go
@@ -25,16 +25,76 @@ func Test_project(t *testing.T) {
 		bindings: nil,
 		want:     nil,
 		wantErr:  false,
+	}, {
+		name: "map index found",
+		key:  "bar",
+		value: map[string]any{
+			"bar": 42,
+		},
+		bindings: nil,
+		want: &projection{
+			result: 42,
+		},
+		wantErr: false,
+	}, {
+		name: "map index found (and nil)",
+		key:  "bar",
+		value: map[string]any{
+			"bar": nil,
+		},
+		bindings: nil,
+		want: &projection{
+			result: nil,
+		},
+		wantErr: false,
+	}, {
+		name: "non string key (not found)",
+		key:  3,
+		value: map[int]any{
+			2: "foo",
+		},
+		bindings: nil,
+		want:     nil,
+		wantErr:  false,
+	}, {
+		name: "non string key (found)",
+		key:  2,
+		value: map[int]any{
+			2: "foo",
+		},
+		bindings: nil,
+		want: &projection{
+			result: "foo",
+		},
+		wantErr: false,
+	}, {
+		name: "non string key (found and nil)",
+		key:  2,
+		value: map[int]any{
+			2: nil,
+		},
+		bindings: nil,
+		want: &projection{
+			result: nil,
+		},
+		wantErr: false,
+	}, {
+		name:     "nil value",
+		key:      "foo",
+		value:    nil,
+		bindings: nil,
+		want:     nil,
+		wantErr:  true,
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := project(context.TODO(), tt.key, tt.value, tt.bindings)
 			if tt.wantErr {
 				tassert.Error(t, err)
-				tassert.Equal(t, tt.want, got)
 			} else {
 				tassert.NoError(t, err)
 			}
+			tassert.Equal(t, tt.want, got)
 		})
 	}
 }
