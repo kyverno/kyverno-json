42154f10dbdcaabb5fee020f3ace4b642bb0cbc5
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-09-19 10:23:55 +0200

    feat: simplify lazy bindings (#489)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/template/binding.go
+++ pkg/engine/template/binding.go
@@ -2,37 +2,14 @@ package template
 
 import (
 	"sync"
-
-	"github.com/jmespath-community/go-jmespath/pkg/binding"
 )
 
-type resolverFunc = func() (any, error)
+type lazyBinding func() (any, error)
 
-type lazyBinding struct {
-	resolver resolverFunc
+func (b lazyBinding) Value() (any, error) {
+	return b()
 }
 
-func (b *lazyBinding) Value() (any, error) {
-	return b.resolver()
-}
-
-func NewLazyBinding(resolver resolverFunc) binding.Binding {
-	binding := &lazyBinding{}
-	lock := &sync.Mutex{}
-	binding.resolver = func() (any, error) {
-		lock.Lock()
-		defer lock.Unlock()
-		value, err := resolver()
-		binding.resolver = func() (any, error) {
-			return value, err
-		}
-		return binding.resolver()
-	}
-	return binding
-}
-
-func NewLazyBindingWithValue(value any) binding.Binding {
-	return NewLazyBinding(func() (any, error) {
-		return value, nil
-	})
+func NewLazyBinding(resolver func() (any, error)) lazyBinding {
+	return sync.OnceValues(resolver)
 }
